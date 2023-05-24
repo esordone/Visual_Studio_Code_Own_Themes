f1d76176e5e9c860908660100d0a5878c794d349
Estrella Ordonez Aguilar <esordone@cbr6s4.42barcelona.com>
2023-05-24 16:14:49 +0200

    Visual Studio Code Themes

---
+++ orange and purple theme/README.md
@@ -0,0 +1,7 @@
+# json-Visual-Studio-42
+
+Extensiones y demás
+
+https://filisantillan.com/blog/vscode-extensiones-configuracion/
+
+<img width="2560" alt="Screen Shot 2023-01-24 at 10 20 37 AM" src="https://user-images.githubusercontent.com/110181623/214254303-e936e5ce-e2c4-42ea-87bd-29ff25e79814.png">
---
+++ orange and purple theme/json.c
@@ -0,0 +1,187 @@
+{
+            "editor.formatOnPaste": false,
+            "editor.formatOnSave": false,
+            "editor.formatOnType": false,
+            "editor.fontFamily": "Victor Mono",
+            "editor.fontLigatures": true,
+            "editor.fontSize": 13,
+            "editor.renderLineHighlight": "none",
+            "editor.occurrencesHighlight": false,
+            "editor.renderWhitespace": "all",
+            "editor.detectIndentation": false,
+            "editor.folding": false,
+            "editor.glyphMargin": false,
+            //"editor.cursorStyle": "line-thin",
+            "editor.cursorBlinking": "blink",
+            //"editor.hideCursorInOverviewRuler": true,
+            "editor.matchBrackets": "never",
+            "editor.colorDecorators": false,
+            "editor.suggestSelection": "first",
+            "editor.minimap.enabled": false,
+            "files.trimFinalNewlines": true,
+            "files.insertFinalNewline": true,
+            "files.trimTrailingWhitespace": true,
+            "workbench.iconTheme": "helium-icon-theme",
+            "editor.tokenColorCustomizations": {
+                "textMateRules": [
+                    {
+                        "scope": [
+                            "storage.modifier.c",
+                            "storage.type.built-in.primitive.c"
+                        ],
+                        "settings": {
+                            "foreground": "#b2c73a"
+                        }
+                    },
+                    {
+                        "scope": [
+                            "string.quoted.double.c"
+                        ],
+                        "settings": {
+                            "foreground": "#ad7dff"
+                        }
+                    },
+                    {
+                        "scope": [
+                            "entity.name.function.preprocessor.c"
+                        ],
+                        "settings": {
+                            "foreground": "#ad7dff"
+                        }
+                    },
+                    {
+                        "scope": [
+                            "string"
+                        ],
+                        "settings": {
+                            "foreground": "#ad7dff"
+                        }
+                    },                    {
+                        "scope": [
+                            "punctuation.definition.string"
+                        ],
+                        "settings": {
+                            "foreground": "#fc951e"
+                        }
+                    },                    {
+                        "scope": [
+                            "constant.character.escape"
+                        ],
+                        "settings": {
+                            "foreground": "#fc951e"
+                        }
+                    },
+                    {
+                        "scope": [
+                            "entity.name.function.c"
+                        ],
+                        "settings": {
+                            "foreground": "#c7c7c7"
+                        }
+                    },
+                    {
+                        "scope": [
+                            "keyword.control.c"
+                        ],
+                        "settings": {
+                            "foreground": "#c17aff"
+                        }
+                    },
+                    {
+                        "scope": [
+                            "variable.parameter"
+                        ],
+                        "settings": {
+                            "foreground": "#7584d1"
+                        }
+                    },
+                    {
+                        "scope": [
+                            "keyword.control.directive.include.c",
+                            "meta.preprocessor.include.c"
+                        ],
+                        "settings": {
+                            "foreground": "#b885ff"
+                        }
+                    },
+                    {
+                        "scope": [
+                            //    "constant.character.escape.c",
+                            "constant.other.placeholder.c"
+                        ],
+                        "settings": {
+                            "foreground": "#ffffff"
+                        }
+                    },
+                    {
+                        "scope": [
+                            "entity.name.function.c"
+                        ],
+                        "settings": {
+                            "foreground": "#879aff"
+                        }
+                    },
+                    {
+                        "scope": [
+                            "constant.numeric"
+                        ],
+                        "settings": {
+                            "foreground": "#d8ff85"
+                        }
+                    },
+                    {
+                        "scope": [
+                            "comment.block.c",
+                            "comment.line.double-slash.c"
+                        ],
+                        "settings": {
+                            "foreground": "#698aa7"
+                        }
+                    }
+                ]
+            },
+            "workbench.colorCustomizations": {
+                "editorGroupHeader.tabsBackground":"#282a36",
+                "tab.border":"#444b70",
+                "editorCursor.foreground":"#2f70ac",
+                "editorLineNumber.activeForeground":"#2f70ac",
+                "activityBar.background": "#282a36",
+                "sideBar.background": "#282a36",
+                "tab.inactiveForeground":"#b1b5cd",
+                "tab.inactiveBackground": "#282a36",
+                "tab.unfocusedActiveForeground":"#b7c0e7",
+                "tab.activeBackground": "#444b70",
+                "editorPane.background": "#794491",
+                "editorBracketHighlight.foreground1": "#54fffc",
+                "editorBracketHighlight.foreground2": "#f7b860",
+                "editorBracketHighlight.foreground3": "#fff59c",
+                "editorBracketHighlight.foreground4": "#b2c73a",
+                "editorBracketHighlight.foreground5": "#fc951e",
+                "editorBracketHighlight.foreground6": "#ad7dff",
+                "editor.background": "#282a36",
+            },
+            "workbench.startupEditor": "newUntitledFile",
+            "javascript.validate.enable": false,
+            "markdown.preview.fontFamily": "Victor Mono",
+            "[markdown]": {
+              //"files.trimTrailingWhitespace": false
+            },
+            "[c]": {
+                "editor.tabCompletion": "off",
+                "editor.wordBasedSuggestions": false,
+                "editor.suggest.insertMode": "replace",
+                //"editor.semanticHighlighting.enabled": true,
+                "editor.tabSize": 4,
+                "editor.insertSpaces": false,
+                "editor.detectIndentation": false,
+                "editor.renderWhitespace": "all",
+                //"files.trimTrailingWhitespace": true,
+                //"files.insertFinalNewline": true,
+            },
+            "workbench.colorTheme": "Moonlight II",
+            "files.autoSave": "afterDelay",
+            "editor.guides.indentation": false,
+            "editor.codeActionsOnSave": {
+
+            },
+          }
